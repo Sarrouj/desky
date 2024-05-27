7d5c854704935d477b6d8ee36e2c3aa67495b475
Sarrouj <zaydssh500@gmail.com>
2024-05-27 15:07:19 +0100

    Feat : auto-entrepreneur Legal INFO UI and Offer Details Page UI

---
+++ client/src/app/(main)/offers/layout.tsx
@@ -0,0 +1,19 @@
+import Footer from "@/Components/layout/footer"
+import Header from "@/Components/layout/Header"  
+
+
+export default function offers({
+    children, // will be a page or nested layout
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+    <>
+        <div className="bg-white border-b-2 ">
+            <Header/>
+        </div>
+        {children}
+        <Footer/>
+    </>
+    )
+}
