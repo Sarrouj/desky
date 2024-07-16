04f8d839167e076c56a0a340d0a511d4276b76d6
Sarrouj <zaydssh500@gmail.com>
2024-07-16 17:57:07 +0100

    Feat: Responsive Offers Page

--- client/src/Components/common/PopoverComponent.tsx
+++ client/src/Components/common/PopoverComponent.tsx
@@ -1,9 +1,8 @@
-
-import * as React from "react"
+import * as React from "react";
 
 // ShadCn UI
-import { Button } from "@/Components/ui/Button"
-import { cn } from "@/lib/utils"
+import { Button } from "@/Components/ui/Button";
+import { cn } from "@/lib/utils";
 import {
   Command,
   CommandEmpty,
@@ -11,84 +10,93 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-} from "@/Components/ui/command"
+} from "@/Components/ui/command";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/Components/ui/popover"
-import { Check, ChevronsUpDown } from "lucide-react"
+} from "@/Components/ui/popover";
+import { Check, ChevronsUpDown } from "lucide-react";
 
 interface data {
-  value : string;
-  label : string;
+  value: string;
+  label: string;
 }
 
-export interface PopoverProps{
-  data : data[];
-  type : string;
-  value : string;
+export interface PopoverProps {
+  data: data[];
+  type: string;
+  value: string;
   setValue: any;
-  search : string;
-  notFound : string;
+  search: string;
+  notFound: string;
 }
 
-const PopoverCom : React.FC<PopoverProps> = ({data, type, value, setValue, search, notFound}) => {
+const PopoverCom: React.FC<PopoverProps> = ({
+  data,
+  type,
+  value,
+  setValue,
+  search,
+  notFound,
+}) => {
   const [open, setOpen] = React.useState<boolean>(false);
 
-  
   return (
     <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
+      <PopoverTrigger asChild>
         <Button
-            variant="outline"
-            role="combobox"
-            aria-expanded={open}
-            className="w-[150px] md:w-[200px] 
-            lg:w-[200px] xl:w-[300px] h-[30px] 
-            md:h-[30px] lg:h-[32px] xl:h-[35px] 
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className=" w-1/2 md:w-[200px]
+            lg:w-[250px] xl:w-[300px] h-[35px] md:h-[32px] xl:h-[35px]
             justify-between border-2
-            text-xs md:text-xs lg:text-sm xl:text-md"
+            text-xs md:text-xs lg:text-sm xl:text-md truncate"
         >
-            {value
-            ? data.find((d) => d.value === value)?.label
-            : type}
-            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          {value ? data.find((d) => d.value === value)?.label : type}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
-        </PopoverTrigger>
-        <PopoverContent className="p-0 w-[150px] md:w-[200px] 
-            lg:w-[200px] xl:w-[300px] ">
+      </PopoverTrigger>
+      <PopoverContent
+        className="p-0 w-[250px] md:w-[250px] 
+            lg:w-[300px] xl:w-[300px] "
+      >
         <Command>
-            <CommandInput placeholder={search} className="text-xs md:text-xs lg:text-sm xl:text-md"/>
-            <CommandEmpty className="text-xs md:text-xs lg:text-sm xl:text-md">{notFound}</CommandEmpty>
-            <CommandGroup>
+          <CommandInput
+            placeholder={search}
+            className="text-xs md:text-xs lg:text-sm xl:text-md"
+          />
+          <CommandEmpty className="text-xs md:text-xs lg:text-sm xl:text-md">
+            {notFound}
+          </CommandEmpty>
+          <CommandGroup>
             <CommandList>
-            {data.map((d, index) => (
+              {data.map((d, index) => (
                 <CommandItem
-                key={index}
-                value={d.value}
-                onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue)
-                    setOpen(false)
-                }}
-                className="text-xs md:text-xs lg:text-sm xl:text-md"
+                  key={index}
+                  value={d.value}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                  }}
+                  className="text-xs md:text-xs lg:text-sm xl:text-md"
                 >
-                <Check
+                  <Check
                     className={cn(
-                    "mr-2 h-4 w-4",
-                    value === d.value ? "opacity-100" : "opacity-0"
+                      "mr-2 h-4 w-4",
+                      value === d.value ? "opacity-100" : "opacity-0"
                     )}
-                />
-                {d.label}
+                  />
+                  {d.label}
                 </CommandItem>
-                
-            ))}
+              ))}
             </CommandList>
-            </CommandGroup>
+          </CommandGroup>
         </Command>
-        </PopoverContent>
+      </PopoverContent>
     </Popover>
-  )
-}
+  );
+};
 
-export default PopoverCom
+export default PopoverCom;
--- client/src/app/[Locale]/(main)/offers/page.tsx
+++ client/src/app/[Locale]/(main)/offers/page.tsx
@@ -3,19 +3,9 @@
 // React Hooks
 import * as React from "react";
 import { useEffect, useState } from "react";
+
 // Components
-import CallToAction from "@/Components/common/CallToAction";
 import OfferCard from "@/Components/layout/OfferCard";
-// ShadCn UI
-// import {
-//     Pagination,
-//     PaginationContent,
-//     PaginationEllipsis,
-//     PaginationItem,
-//     PaginationLink,
-//     PaginationNext,
-//     PaginationPrevious,
-//   } from "@/Components/ui/pagination"
 import {
   Select,
   SelectContent,
@@ -49,6 +39,7 @@ const Offers: React.FC = () => {
   const [cityValue, citySetValue] = React.useState<string>("");
   const [selectedValue, setSelectedValue] = React.useState<string>("Oldest");
   const [sortedData, setSortedData] = React.useState<any>([]);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
   // filters UI
   let [filter, setFilter] = useState<(string | number)[]>([]);
@@ -146,6 +137,7 @@ const Offers: React.FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchedData]);
 
+
   return (
     <>
       <div className="bg-white border-b-2 ">
@@ -157,20 +149,37 @@ const Offers: React.FC = () => {
           AboutUS={"hover:text-primary"}
         />
       </div>
-      <main className="py-10 px-10 bg-neutralBg text-secondaryDarkBlue h-full">
-        <section className="pt-5">
-          <div className="flex justify-between items-center">
-            <h1 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl  font-semibold">
+      <main className="sm:py-4 md:py-5 px-3 sm:px-8 md:px-10 bg-neutralBg text-secondaryDarkBlue h-full">
+        <section className="sm:pt-5">
+          <div className="flex md:text-start md:justify-between items-center text-center justify-center w-full">
+            <h1 className="text-xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold hidden sm:block">
               {OffersPageContent("Title")}
             </h1>
-            {userRole !== null ? (
-              userRole == "depositor" ? (
-                <Link href={`/${Language}/Create-Offer`}>
-                  <Button className="text-white">
-                    {OffersPageContent("CallToAction")}
-                  </Button>
-                </Link>
-              ) : (
+            <div className="hidden md:block">
+              {userRole !== null ? (
+                userRole == "depositor" ? (
+                  <Link href={`/${Language}/Create-Offer`}>
+                    <Button className="text-white text-xs">
+                      {OffersPageContent("CallToAction")}
+                    </Button>
+                  </Link>
+                ) : (
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger className="bg-orange-200 text-white py-3 px-4 text-sm rounded-md">
+                        {OffersPageContent("CallToAction")}
+                      </TooltipTrigger>
+                      <TooltipContent
+                        side="top"
+                        className="text-xs text-center"
+                      >
+                        {OffersPageContent("CallToActionPopMsg1")} <br />{" "}
+                        {OffersPageContent("CallToActionPopMsg2")}
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                )
+              ) : userRole == null && status == "unauthenticated" ? (
                 <TooltipProvider>
                   <Tooltip>
                     <TooltipTrigger className="bg-orange-200 text-white py-3 px-4 text-sm rounded-md">
@@ -182,89 +191,79 @@ const Offers: React.FC = () => {
                     </TooltipContent>
                   </Tooltip>
                 </TooltipProvider>
-              )
-            ) : userRole == null && status == "unauthenticated" ? (
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger className="bg-orange-200 text-white py-3 px-4 text-sm rounded-md">
-                    {OffersPageContent("CallToAction")}
-                  </TooltipTrigger>
-                  <TooltipContent side="top" className="text-xs text-center">
-                    {OffersPageContent("CallToActionPopMsg1")} <br />{" "}
-                    {OffersPageContent("CallToActionPopMsg2")}
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            ) : (
-              <div className="flex flex-col gap-2 items-end">
-                <Skeleton className="w-40 h-3 bg-gray-200" />
-                <Skeleton className="w-32 h-3 bg-gray-200" />
-              </div>
-            )}
+              ) : (
+                <div className="flex flex-col gap-2 items-end">
+                  <Skeleton className="w-40 h-3 bg-gray-200" />
+                  <Skeleton className="w-32 h-3 bg-gray-200" />
+                </div>
+              )}
+            </div>
           </div>
           <div className="mt-8 flex justify-between items-end">
-            <div className="">
-              <div className="flex gap-2">
+            <div className=" w-full">
+              <div className="flex flex-col md:flex-row gap-2 md:justify-center lg:justify-start">
                 <Input
                   placeholder={OffersPageContent("SearchPlaceholder")}
-                  className="w-[200px] md:w-[250px] lg:w-[300px] xl:w-[400px]
-                              h-[30px] md:h-[30px] lg:h-[32px] xl:h-[35px]  border-2 text-xs md:text-xs lg:text-sm xl:text-md
+                  className=" lg:w-[380px] xl:w-[500px]
+                              h-[35px] md:h-[32px] xl:h-[35px] border-2 text-xs md:text-xs lg:text-sm xl:text-md
                               "
                   value={searchValue}
                   onChange={(e) => setSearchValue(e.target.value)}
                 />
-                {Language == "en" ? (
-                  <>
-                    <PopoverCom
-                      data={CategoriesEN}
-                      type={"Categories"}
-                      value={categoryValue}
-                      setValue={categorySetValue}
-                      search={"Search..."}
-                      notFound={"No Category found."}
-                    />
-                    <PopoverCom
-                      data={CitiesEN}
-                      type={"Location"}
-                      value={cityValue}
-                      setValue={citySetValue}
-                      search={"Search..."}
-                      notFound={"No City found."}
-                    />
-                  </>
-                ) : (
-                  <>
-                    <PopoverCom
-                      data={CategoriesFR}
-                      type={"Catégories"}
-                      value={categoryValue}
-                      setValue={categorySetValue}
-                      search={"Recherche..."}
-                      notFound={"Aucune catégorie trouvée."}
-                    />
-                    <PopoverCom
-                      data={CitiesFR}
-                      type={"Ville"}
-                      value={cityValue}
-                      setValue={citySetValue}
-                      search={"Recherche..."}
-                      notFound={"Aucune ville trouvée."}
-                    />
-                  </>
-                )}
+                <div className="flex">
+                  {Language == "en" ? (
+                    <div className="flex gap-2 w-full">
+                      <PopoverCom
+                        data={CategoriesEN}
+                        type={"Categories"}
+                        value={categoryValue}
+                        setValue={categorySetValue}
+                        search={"Search..."}
+                        notFound={"No Category found."}
+                      />
+                      <PopoverCom
+                        data={CitiesEN}
+                        type={"Location"}
+                        value={cityValue}
+                        setValue={citySetValue}
+                        search={"Search..."}
+                        notFound={"No City found."}
+                      />
+                    </div>
+                  ) : (
+                    <div className="flex gap-2 w-full">
+                      <PopoverCom
+                        data={CategoriesFR}
+                        type={"Catégories"}
+                        value={categoryValue}
+                        setValue={categorySetValue}
+                        search={"Recherche..."}
+                        notFound={"Aucune catégorie trouvée."}
+                      />
+                      <PopoverCom
+                        data={CitiesFR}
+                        type={"Ville"}
+                        value={cityValue}
+                        setValue={citySetValue}
+                        search={"Recherche..."}
+                        notFound={"Aucune ville trouvée."}
+                      />
+                    </div>
+                  )}
+                </div>
               </div>
               <ul className="flex items-center gap-2 text-sm mt-2">
                 {filter.map((act, index) => (
                   <li
                     key={index}
-                    className="bg-orange-300 text-white py-1.5 px-3.5 rounded-full cursor-pointer"
+                    className="bg-orange-300 text-white py-1 px-3 lg:py-1.5 lg:px-3.5 rounded-full cursor-pointer"
                   >
-                    <p className="text-sm text-white">{act}</p>
+                    <p className="text-xs lg:text-sm text-white">{act}</p>
                   </li>
                 ))}
                 {filter.length !== 0 ? (
                   <li
-                    className="cursor-pointer text-primaryOrange font-semibold text-sm "
+                    className="cursor-pointer text-primaryOrange font-semibold text-xs lg:text-sm"
                     onClick={() => clearFilters()}
                   >
                     {OffersPageContent("ClearFilters")}
@@ -285,14 +284,16 @@ const Offers: React.FC = () => {
               )}
             </div>
             <div className="flex items-center gap-2">
-              <p className="text-xs">{OffersPageContent("SortBy")}</p>
+              <p className="text-xs hidden md:block">
+                {OffersPageContent("SortBy")}
+              </p>
               <Select onValueChange={handleSorting}>
                 <SelectTrigger className="w-[80px] md:w-[90px] xl:w-[90px] h-[28px] md:h-[28px] lg:h-[30px] xl:h-[32px] border-2 text-[10px] md:text-xs">
                   <SelectValue placeholder={OffersPageContent("Oldest")}>
                     {selectedValue}
                   </SelectValue>
                 </SelectTrigger>
-                <SelectContent>
+                <SelectContent className="mr-5">
                   <SelectItem value="Newest" className="text-xs">
                     {OffersPageContent("Newest")}
                   </SelectItem>
@@ -303,74 +304,56 @@ const Offers: React.FC = () => {
               </Select>
             </div>
           </div>
-          <div className="flex justify-start items-start flex-wrap gap-6 w-full min-h-screen">
+          {/* <div className="grid grid-cols-1 lg:grid-cols-2 grid-flow-row content-start gap-3 lg:gap-5 xl:gap-6 w-full min-h-screen"> */}
             {offersIsLoadig === false ? (
               sortedData.length !== 0 ? (
-                sortedData.map((offer: any, index: number) =>
-                  offer.offer_state === "open" ? (
-                    <OfferCard
-                      key={offer._id}
-                      title={offer.offer_title}
-                      date={offer.offer_deadLine}
-                      location={offer.offer_location}
-                      budget={offer.offer_budget}
-                      Category={offer.offer_category}
-                      Desc={offer.offer_description}
-                      offerNumber={index + 1}
-                      Proposals={offer.offer_apply}
-                      id={offer._id}
-                      lg={Language}
-                      Content={OfferContent}
-                    />
-                  ) : null
-                )
+                <div className="grid grid-cols-1 lg:grid-cols-2 grid-flow-row content-start gap-3 lg:gap-5 w-full min-h-screen">
+                  {
+                    sortedData.map((offer: any, index: number) =>
+                      offer.offer_state === "open" ? (
+                        <OfferCard
+                          key={offer._id}
+                          title={offer.offer_title}
+                          date={offer.offer_deadLine}
+                          location={offer.offer_location}
+                          budget={offer.offer_budget}
+                          Category={offer.offer_category}
+                          Desc={offer.offer_description}
+                          offerNumber={index + 1}
+                          Proposals={offer.offer_apply}
+                          id={offer._id}
+                          lg={Language}
+                          Content={OfferContent}
+                        />
+                      ) : null
+                    )
+                  }
+                </div>
               ) : filter.length !== 0 ? (
-                <div className="container mx-auto px-4 py-20 text-center">
-                  <h2 className="text-2xl font-bold mb-4">
+                <div className="container mx-auto px-4 py-28 text-center min-h-screen flex flex-col items-center">
+                  <h2 className="text-lg md:text-xl lg:text-2xl font-bold mb-1 lg:mb-3 xl:mb-4">
                     {OffersPageContent("NotFoundMsg")}
                   </h2>
-                  <p className="text-gray-600">
+                  <p className="text-gray-600 max-w-[900px] text-xs sm:text-sm md:text-md lg:text-lg">
                     {OffersPageContent("NotFoundMsgDesc")}
                   </p>
                 </div>
               ) : (
-                Array.from({ length: 10 }).map((_, index) => (
+               <div className="grid grid-cols-1 lg:grid-cols-2 grid-flow-row content-start gap-3 lg:gap-5 xl:gap-6 w-full min-h-screen">
+                { Array.from({ length: 10 }).map((_, index) => (
                   <OfferCardSkeleton key={index} />
-                ))
+                ))}
+               </div>
               )
             ) : (
-              Array.from({ length: 10 }).map((_, index) => (
-                <OfferCardSkeleton key={index} />
-              ))
+              <div className="grid grid-cols-1 lg:grid-cols-2 grid-flow-row content-start gap-3 lg:gap-5 xl:gap-6 w-full min-h-screen">
+                { Array.from({ length: 10 }).map((_, index) => (
+                  <OfferCardSkeleton key={index} />
+                ))}
+               </div>
             )}
-          </div>
+          {/* </div> */}
         </section>
-        {/* <section className="mt-5 flex justify-end">
-                <div>
-                    <Pagination>
-                        <PaginationContent className="gap-1">
-                            <PaginationItem >
-                                <PaginationPrevious href="#" className="text-xs"/>
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationLink href="#" className="text-white text-xs bg-primaryOrange w-8 h-8 rounded-full p-1">1</PaginationLink>
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationLink href="#" className="text-primaryOrange text-xs rounded-full w-8 h-8">2</PaginationLink>
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationLink href="#" className="text-primaryOrange text-xs rounded-full w-8 h-8">3</PaginationLink>
-                            </PaginationItem>
-                            <PaginationItem>
-                            <PaginationEllipsis />
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationNext href="#" className="text-xs"/>
-                            </PaginationItem>
-                        </PaginationContent>
-                    </Pagination>
-                </div>
-            </section> */}
       </main>
     </>
   );
