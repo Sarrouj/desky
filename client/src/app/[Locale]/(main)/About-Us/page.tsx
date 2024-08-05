ad1ecd6ff64f3b30814ec0cf8aeb8833a6b8d0ca
Sarrouj <zaydssh500@gmail.com>
2024-08-05 21:00:14 +0100

    Feat : About Internationalization

--- client/src/app/[Locale]/(main)/About-Us/page.tsx
+++ client/src/app/[Locale]/(main)/About-Us/page.tsx
@@ -4,6 +4,7 @@ import Header from "@/Components/layout/Header";
 import { useTranslations } from "next-intl";
 function AboutUs() {
   const NavbarContent = useTranslations("NavBar");
+  const Content = useTranslations("AboutUs");
   return (
     <>
       <Header
@@ -14,7 +15,7 @@ function AboutUs() {
         AboutUS={"hover:text-primary"}
       />
       <div className="bg-neutralBg text-secondaryDarkBlue min-h-screen w-full flex flex-col justify-center items-center text-center">
-        <p className="text-lg md:text-xl lg:text-2xl font-bold mb-16">Steal In Progress...</p>
+        <p className="text-lg md:text-xl lg:text-2xl font-bold mb-16">{Content('Title')}</p>
       </div>
     </>
   );
